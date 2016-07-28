6cb42be96815220add8beb63b105b54fa967381c
miho1202 <miho1202@student.miun.se>
2016-07-28 21:49:50 +0200

    rearrange comments as PEP8

--- src/gameobjects/GameObject.py
+++ src/gameobjects/GameObject.py
@@ -1,50 +1,66 @@
-"""
-graphic object used in gameplay
 
-author Mikael Holmbom
-ver 1.0
-"""
+
 class GameObject:
+	"""
+	graphic object used in gameplay
+
+	author Mikael Holmbom
+	ver 1.0
+	"""
 
 	# this current sprite
 	__sprite	= None
 	# this current rectangle
 	__rect		= None
-
+	# this current position
 	__pos		= [0,0]
 
-
 	def __init__(self, sprite):
-		#add args sprite and rect
+		# add args sprite and rect
 		self.__sprite 	= sprite
 		self.__rect 	= sprite.get_rect()
-		
-	# set current sprite
+
 	def set_sprite(self, sprite):
+		"""
+		set current sprite
+		"""
 		self.__sprite = sprite
-	# set current rect
+
 	def set_rect(self, rect):
+		"""
+		set current rectangle
+		"""
 		self.__rect = rect
+
 	def set_pos(self, x, y):
+		"""
+		set current position
+		"""
 		self.__pos[0] = x
 		self.__pos[1] = y
 
-	# get current sprite
 	def get_sprite(self):
+		"""
+		get current sprite
+		"""
 		return self.__sprite
 
-	"""
-	get current rect
-	"""
 	def get_rect(self):
+		"""
+		get current rect
+		"""
 		return self.__rect
 
 	def get_pos(self):
+		"""
+		get this position
+		"""
 		return self.__pos
-	"""
-	get the centerposition of this rectangle
-	"""
+
 	def get_centerpos(self):
+		"""
+		get the centerposition of this rectangle
+		"""
 		x = self.get_pos()[0] - self.get_rect().width / 2
 		y = self.get_pos()[1] - self.get_rect().height / 2
 
