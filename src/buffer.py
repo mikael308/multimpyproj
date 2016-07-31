52e0d34ceed61028b6e8daef1dc0eb97fb06acaa
miho1202 <miho1202@student.miun.se>
2016-07-31 15:59:53 +0200

    init

---
+++ src/buffer.py
@@ -0,0 +1,93 @@
+
+
+class Buffer:
+    """
+    :author: Mikael Holmbom
+    :ver: 1.0
+    """
+
+    __elements      = {}
+    __capacity      = 0
+    __size          = 0
+
+    __idx           = 0
+
+    def __init__(self, capacity):
+        if capacity <= 0:
+            raise ValueError("buffer capacity must be a positive number")
+
+        self.__capacity = capacity
+
+    def get_capacity(self):
+        """
+        get the capacity of this Buffer
+        :return:
+        """
+        return self.__capacity
+
+    def get_size(self):
+        """
+        get size of this Buffer, that is current amount of elements
+        :return:
+        """
+        return self.__size
+
+    def index(self, item):
+        """
+        get the item element index from this Buffer
+        :param item: item to determine index
+        :return: the index of item, if item is not found None is returned
+        """
+        for e in self.__elements.items():
+            key = e[0]
+            value = e[1]
+            if value == item:
+                return key
+
+    def add(self, item):
+        """
+        add item to this Buffer
+        :param item: item to add
+        :return: True if item was added
+        """
+        for i in range(0, self.__capacity):
+            idx = self.__idx % self.__capacity
+            self.__idx += 1
+
+            if not self.__elements.has_key(idx):
+                self.__elements[idx] = item
+                self.__size += 1
+                return True
+
+        return False
+
+    def delete(self, elem):
+        """
+        delete element from this Buffer
+        :param elem: element to delete
+        :return: True if element was deleted
+        """
+        for item in self.__elements.items():
+            key = item[0]
+            value = item[1]
+            if value == elem:
+                del self.__elements[key]
+
+                self.__size -= 1
+                return True
+
+        return False
+
+    def is_empty(self):
+        """
+        determine if this Buffer is empty, that is has 0 elements
+        :return: True if empty
+        """
+        return self.__size == 0
+
+    def is_full(self):
+        """
+        determine if this Buffer is full, that is reached its max capacity
+        :return:
+        """
+        return self.__size == self.__capacity
