9feaf6f8fb43161bd7457639c4dfe3ceebe043f8
miho1202 <miho1202@student.miun.se>
2016-08-01 13:40:42 +0200

    update player assignment: use resource to get values, rename to pl

--- src/main.py
+++ src/main.py
@@ -6,10 +6,11 @@
 	author: Mikael Holmbom
 	ver 1.0
 """
+import resource
 from gameengine import GameEngine
 from gameobjects.player import Player
 
-player	= Player.Player("mikael", 3)
+player = Player("mikael", resource.get_value("player_hp_start"), resource.get_value("player_mov_speed"))
 
 ge = GameEngine()
 ge.add_player(player)
