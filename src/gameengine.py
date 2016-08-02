927e236aaea42ba8ac7b48155ffdd3f1fe8dd199
miho1202 <miho1202@student.miun.se>
2016-08-02 16:33:16 +0200

    set resource buf capacity directly to buffer init args

--- src/gameengine.py
+++ src/gameengine.py
@@ -42,8 +42,7 @@ class GameEngine:
 
 	__end_state			= 0
 	__packets			= []
-	__buf_capacity 		= resource.get_value("buf_capacity")
-	__buf				= Buffer(__buf_capacity)
+	__buf				= Buffer(resource.get_value("buf_capacity"))
 
 	def __init__(self):
 		"""
@@ -70,6 +69,61 @@ class GameEngine:
 		:return:
 		"""
 		self.__player = player
+
+	def run(self):
+		"""
+		start this gameengines gameloop\n
+		prior -- player must be defined, see add_player(self)
+		:return:
+		"""
+		try:
+			self.__start()
+
+		except Exception as e:
+			traceback.print_exc()
+			self.__destruct()
+
+	def __start(self):
+		"""
+		the startpoint of this gameengine
+		:return:
+		"""
+		pygame.init()
+
+		size = (resource.get_dimen("main_window_size_width"),
+				resource.get_dimen("main_window_size_height"))
+
+		self.__setup_gameobjects()
+		self.__screen = Screen(size)
+		self.__screen\
+			.set_player(self.__player)\
+			.set_buffer(self.__buf)\
+			.set_cpus(self.__cpus) \
+			.set_packets(self.__packets)
+		self.__screen.setup()
+
+		# set key press repeat instantly as standard
+		flag_key_repeat_inst = True
+		pygame.key.set_repeat(21,21)
+
+		self.__game_loop = True
+		self.__clock = pygame.time.Clock()
+		self.__time = 0
+		# GAME LOOP ####################################
+		while self.__game_loop:
+			for event in pygame.event.get():
+				self.__handle_event(event)
+
+			self.__gamelogic()
+
+			self.__screen.render()
+
+		# ! GAME LOOP ###################################
+
+		self.__update_endstate()
+		self.__validate_endstate()
+
+		self.__destruct()
 
 	@staticmethod
 	def load_image(img_id):
