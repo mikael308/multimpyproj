13057f7149f1b735d1f507a3c89277da1fb2e070
miho1202 <miho1202@student.miun.se>
2016-08-03 18:34:56 +0200

    init soundengine

---
+++ src/soundengine.py
@@ -0,0 +1,45 @@
+import pygame
+import resource
+from settings import settings
+
+
+class SoundEngine:
+    """
+    facade controlling sounds\n
+    reads soundfiles from resourcefile: sounds.xml\n
+    * use play(self, id) to play sound with id as matching attribute name from resource item
+    * close() to release resource\n
+    :author: Mikael Holmbom
+    :version: 1.0
+    """
+
+    # collection of sounds from resource file
+    __sounds = {}
+
+    def __init__(self):
+        pygame.mixer.init()
+
+        res = resource.get_soundsrc()
+
+        for key, val in res:
+            filename = res.get_rootdir() + val
+            self.__sounds[key] = pygame.mixer.Sound(filename)
+
+    def close(self):
+        """
+        close this and release resources
+        :return:
+        """
+        pygame.mixer.quit()
+
+    def play(self, id):
+        """
+        play sound of id param\n
+        id must match with attrib name in resource item in resource file\n
+        settings soundfx must be enabled to successfully play
+        :param id:
+        :type id: string
+        :return:
+        """
+        if settings.is_soundfx_enabled():
+            self.__sounds[id].play()
