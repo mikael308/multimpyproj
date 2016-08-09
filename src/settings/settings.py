75568fc826bdbbd30fe7072129d822554dc93f29
miho1202 <miho1202@student.miun.se>
2016-08-10 01:15:57 +0200

    add fps and holdtime values

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -4,10 +4,12 @@ settings access functions
 :author: Mikael Holmbom
 :version: 1.0
 """
-
+import src.resource
 
 __soundfx_enabled = True
 
+__FPS = src.resource.get_value("fps")
+__holdtime = 1000 / __FPS
 
 def is_soundfx_enabled():
     return __soundfx_enabled
@@ -20,3 +22,19 @@ def switch_soundfx_enabled():
     """
     __soundfx_enabled = not is_soundfx_enabled()
     print "soundfx is " + str(__soundfx_enabled)
+
+
+def get_FPS():
+    """
+    get FPS value as of resource
+    :return:
+    """
+    return __FPS
+
+
+def get_holdtime():
+    """
+    get the holdtime, that is 1 000ms / FPS
+    :return:
+    """
+    return __holdtime
