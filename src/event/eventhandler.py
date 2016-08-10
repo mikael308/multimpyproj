ca68ffdbb900d22a5fdaf1daef0a672c14f57f9c
miho1202 <miho1202@student.miun.se>
2016-08-10 23:49:45 +0200

    init module eventhandler

---
+++ src/event/eventhandler.py
@@ -0,0 +1,44 @@
+
+
+class EventHandler:
+    """
+    EventHandler defines procedures of handling incoming pygame.event.Event instances and parse them into use-case logic\n
+    Depending on the incoming event, the EventHandler can deligate tasks to a Interface\n
+    :See: _get_interface()
+
+    :author: Mikael Holmbom
+    :version: 1.0
+    """
+
+    __interface         = None
+
+    def __init__(self, interface):
+        """
+
+        :param interface:
+        :type interface: src.interface.Interface
+        """
+        self.__interface = interface
+
+    def _get_interface(self):
+        """
+        gets this interface\n
+        :return:
+        """
+        return self.__interface
+
+    def handle(self, event):
+        """
+        handle a incoming event\n
+        :param event: single event to handle
+        :type event: pygame.event.Event
+        :return: None
+        """
+        raise NotImplemented("abstract method not implemented")
+
+    def repeated_tasks(self):
+        """
+        tasks which proceed independent of events\n
+        :return:
+        """
+        pass
