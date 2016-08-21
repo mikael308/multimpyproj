742c4fa8631ca10ea7d96c212619c0268175ff94
miho1202 <miho1202@student.miun.se>
2016-08-22 01:48:21 +0200

    init module navigation_controls

---
+++ src/settings/navigation_controls.py
@@ -0,0 +1,17 @@
+"""
+
+	metadata containing controls used for navigation\n
+	
+	:author: Mikael Holmbom
+	:ver: 1.0
+"""
+import pygame
+
+
+key_info			= pygame.K_i
+key_quit			= pygame.K_ESCAPE
+key_back            = pygame.K_BACKSPACE
+
+
+
+
