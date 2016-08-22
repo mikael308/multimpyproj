348e137eaeb73faec410cba0565e81d7c659752e
miho1202 <miho1202@student.miun.se>
2016-08-22 02:05:08 +0200

    init result_eventhandler module

---
+++ src/event/result_eventhandler.py
@@ -0,0 +1,33 @@
+import pygame
+import src.settings.navigation_controls as nav_controls
+from eventhandler import EventHandler
+
+
+class ResultEventHandler(EventHandler):
+    """
+    eventhandler for use-case: result\n
+
+    :author: Mikael Holmbom
+    :version: 1.0
+    """
+
+    def handle(self, event):
+        interf = self._get_interface()
+
+        if event.type == pygame.QUIT:
+            if interf is not None:
+                interf.get_engine().shutdown()
+                pygame.quit()
+
+        if event.type == pygame.KEYDOWN:
+            key = pygame.key.get_pressed()
+
+            if key[nav_controls.key_quit]:
+                interf.get_engine().shutdown()
+                pygame.quit()
+
+            elif key[nav_controls.key_back]:
+                interf.get_engine().shutdown()
+
+            interf.get_engine().shutdown()
+
