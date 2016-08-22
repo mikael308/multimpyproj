1936f61f749c21b24cd196d36123df154efaefd8
miho1202 <miho1202@student.miun.se>
2016-08-22 13:47:37 +0200

    init module info_interface

---
+++ src/interface/info_interface.py
@@ -0,0 +1,25 @@
+from interface import Interface
+from src.engine.info_engine import InfoEngine
+from src.event.info_eventhandler import InfoEventHandler
+from src.output.info_output import InfoOutput
+
+
+class InfoInterface(Interface):
+    """
+    Interface for use-case: info\n
+
+    :author: Mikael Holmbom
+    :version: 1.0
+    """
+
+    def __init__(self):
+        """
+
+        """
+        eh = InfoEventHandler(self)
+        e = InfoEngine()
+        out = InfoOutput()
+
+        self._set_engine(e)
+        self._set_eventhandler(eh)
+        self._set_output(out)
