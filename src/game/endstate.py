00916f28b8924e83888ff76660f033c79d22aed6
miho1202 <miho1202@student.miun.se>
2016-08-26 14:49:23 +0200

    init module endstate

---
+++ src/game/endstate.py
@@ -0,0 +1,10 @@
+from src.activable import Activable
+
+
+class EndState(Activable):
+    """
+    endstate is in state active if the endstate is valid\n
+    """
+
+    score   = 0
+    level   = 0
