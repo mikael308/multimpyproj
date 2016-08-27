e315e2c52a65b17100f0a2cee3a712bfb266a7b1
miho1202 <miho1202@student.miun.se>
2016-08-28 01:00:51 +0200

    endstate: add class docstrings

--- src/game/endstate.py
+++ src/game/endstate.py
@@ -4,6 +4,9 @@ from src.activable import Activable
 class EndState(Activable):
     """
     endstate is in state active if the endstate is valid\n
+
+    :author: Mikael Holmbom
+    :version: 1.0
     """
 
     score   = 0
