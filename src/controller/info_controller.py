2d61e733c0dafd2fcce17bcf448b462d366fb919
miho1202 <miho1202@student.miun.se>
2016-08-28 01:06:08 +0200

    info_controller: add class docstring

--- src/controller/info_controller.py
+++ src/controller/info_controller.py
@@ -2,6 +2,12 @@ from src.controller.controller import Controller
 
 
 class InfoController(Controller):
+    """
+    controller for use-case: info\n
+
+    :author: Mikael Holmbom
+    :version: 1.0
+    """
 
     def __init__(self):
         """
