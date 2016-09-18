bbe2679ca6964b9e39e3147fe87b61d861df0257
miho1202 <miho1202@student.miun.se>
2016-09-18 13:14:20 +0200

    main: set the display caption

--- src/main.py
+++ src/main.py
@@ -12,11 +12,14 @@ import sys, os
 path = os.getcwd() 
 sys.path.append(path)
 
+import pygame
+import src.resource.resource as resource
 from src.interface.startmenu_interface import StartMenuInterface
 
 
 def start_game():
     i = StartMenuInterface()
+    pygame.display.set_caption(resource.get_string("game_title"))
     i.maindisplay()
 
 
