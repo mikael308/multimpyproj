1a5664741909a0657cbb406ebc6350745a1c7b6c
miho1202 <miho1202@student.miun.se>
2016-09-19 19:14:30 +0200

    init testcase

---
+++ test/testcase.py
@@ -0,0 +1,53 @@
+import sys
+import os
+
+sys.path.append(os.getcwd())
+
+
+class TestCase:
+	"""
+	Testcase used in testsuite\n
+	to implement testcase\n
+	define everything needed to be tested in function _run(self)\n
+	use module test.tools to assert values\n
+	\n
+	to run testcase: see function run_test(self)\n
+
+	:author: Mikael Holmbom
+	:version: 1.0
+	"""
+
+	# the name of this testcase
+	__name = ""
+
+	def __init__(self, name):
+		self.__name = name
+
+	def _run(self):
+		"""
+		defines all testing for this testcase\n
+		"""
+		raise NotImplementedError("test not implemented")
+
+	def run_test(self):
+		"""
+		run this testcase\n
+		prints out the result\n
+		"""
+		try:
+			self._run()
+
+			TestCase._print_success("+ ["+self.__name +"] test PASSED")
+
+		except Exception as e:
+			s = "- ["+self.__name +"] test FAILED"
+			s = s+ e.message
+			TestCase._print_fail(s)
+			
+	@staticmethod
+	def _print_fail(message):
+		print('\x1b[6;30;41m' + message + '\x1b[0m')
+
+	@staticmethod
+	def _print_success(message):
+		print('\x1b[6;30;42m' + message + '\x1b[0m')
